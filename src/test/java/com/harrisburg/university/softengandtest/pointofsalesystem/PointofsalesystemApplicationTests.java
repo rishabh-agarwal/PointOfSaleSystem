ec8abae5dba1a5e5241668cd72e2967ba02437ff
Rishabh Agarwal <rishabhagarwal@Rishabh-MacBook-Pro.local>
2021-04-15 22:16:24 -0500

    chore: init changes

---
+++ src/test/java/com/harrisburg/university/softengandtest/pointofsalesystem/PointofsalesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harrisburg.university.softengandtest.pointofsalesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointofsalesystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
